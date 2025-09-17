ea2b51b9c131e83e8e7cc292567b013c1049eb89
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2025-09-17 23:54:39 +0200

    return a basic json response

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,36 @@
-use axum::{Router, routing::get};
+use axum::{Router, debug_handler, response::Json, routing::get};
+use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new().route("/threads", get(get_threads));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Post {
+    id: String,   // OID?
+    name: String, // poster name
+    subject: String,
+    content: String,
+    media_url: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Thread {
+    posts: Vec<Post>,
+}
+
+#[debug_handler]
+async fn get_threads() -> Json<Thread> {
+    Json(Thread {
+        posts: vec![Post {
+            id: "1".to_string(),
+            name: "anon".to_string(),
+            subject: "test".to_string(),
+            content: "hello, world".to_string(),
+            media_url: "https://example.com/".to_string(),
+        }],
+    })
 }
