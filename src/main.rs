2813009a41615dc516b4c4a12268c315df4b6b7b
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2025-09-24 22:06:22 +0200

    add basic db setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use std::env;
+
+use sqlx::postgres::PgPoolOptions;
+
 mod board;
 mod file;
 mod routing;
@@ -5,6 +9,15 @@ mod thread;
 
 #[tokio::main]
 async fn main() {
+    let db_url =
+        env::var("DATABASE_URL").expect("Env var DATABASE_URL is required for this service.");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(db_url.as_str())
+        .await
+        .unwrap();
+
+    sqlx::migrate!().run(&pool).await;
     let app_routes = routing::build_routes();
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app_routes).await.unwrap();
