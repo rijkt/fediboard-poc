f6dd55846210283e612b61980c02b2d6e03384b4
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2025-10-02 20:57:12 +0200

    remove test code

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use sqlx::postgres::PgPoolOptions;
 
-use crate::board::Board;
-
 mod board;
 mod file;
 mod routing;
@@ -12,22 +10,11 @@ async fn main() {
     let db_url =
         dotenvy::var("DATABASE_URL").expect("Env var DATABASE_URL is required for this service.");
 
-    let pool = PgPoolOptions::new()
+    let _ = PgPoolOptions::new()
         .max_connections(5)
         .connect(db_url.as_str())
         .await
         .unwrap();
-
-    let boards = sqlx::query_as!(
-        Board,
-        r#"
-            select board_id, name
-            from board
-        "#
-    )
-    .fetch_all(&pool)
-    .await;
-    println!("{:?}", boards);
 
     let app_routes = routing::build_routes();
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
