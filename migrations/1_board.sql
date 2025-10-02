b0a2ead55917faa49549d872225bcae422f7c2cf
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2025-10-02 20:41:06 +0200

    expand migration

--- migrations/1_board.sql
+++ migrations/1_board.sql
@@ -1,4 +1,12 @@
-create table "board" (
-    board_id       uuid primary key default gen_random_uuid(),
-    name           text unique not null
-);
+create table
+    "board" (
+        board_id uuid primary key default gen_random_uuid(),
+        name text unique not null
+    );
+
+insert into
+    board (name)
+values
+    ('a'),
+    ('b'),
+    ('c');
