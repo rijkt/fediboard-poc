b4c2be0414df57218721b0f13e428300b6a3cd84
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2025-10-03 19:51:29 +0200

    push handlers into submodule

--- src/thread.rs
+++ src/thread.rs
@@ -1,14 +1,11 @@
+mod handler;
 mod post;
 mod thread;
 
-use crate::thread::post::{Post, mock_post};
-use crate::thread::thread::{Thread, mock_thread};
-use axum::Router;
-use axum::response::Json;
-use axum::routing::{get, post};
-use axum::{Form, extract::Path};
+use axum::{Router, routing::get, routing::post};
 use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
+
+use crate::thread::handler::{create_thread, get_post, get_posts, get_thread, get_threads};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub(crate) struct PostCreation {
@@ -25,47 +22,4 @@ pub(crate) fn routes() -> Router {
         .route("/{thread_id}", get(get_thread))
         .route("/{thread_id}/posts", get(get_posts))
         .route("/{thread_id}/posts/{post_id}", get(get_post))
-}
-
-async fn get_threads(Path(params): Path<HashMap<String, String>>) -> Json<Vec<Thread>> {
-    let _board_name = params
-        .get("board_name")
-        .expect("board_name is required to get all threads");
-    let thread = mock_thread();
-    Json(vec![thread])
-}
-
-async fn get_thread(Path(params): Path<HashMap<String, String>>) -> Json<Thread> {
-    let _board_name = params
-        .get("board_name")
-        .expect("board_name is required to get all threads");
-    let _thread_id = params.get("thread_id");
-    Json(mock_thread())
-}
-
-async fn create_thread(Form(post_creation): Form<PostCreation>) -> Json<Thread> {
-    let original_post = Post {
-        id: "1".to_string(),
-        name: post_creation.name,
-        subject: post_creation.subject,
-        content: post_creation.content,
-        media_url: post_creation.media_url,
-    };
-    Json(Thread {
-        id: "1".to_owned(),
-        board_id: "1".to_owned(),
-        posts: vec![original_post],
-    })
-}
-
-async fn get_posts(Path(params): Path<HashMap<String, String>>) -> Json<Vec<Post>> {
-    let _thread_id = params.get("thread_id");
-    let _post_id = params.get("post_id");
-    Json(vec![mock_post()])
-}
-
-async fn get_post(Path(params): Path<HashMap<String, String>>) -> Json<Post> {
-    let _thread_id = params.get("thread_id");
-    let _post_id = params.get("post_id");
-    Json(mock_post())
 }
