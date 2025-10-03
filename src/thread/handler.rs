922ae67b7a892950c9f4bf417f244670a8a52754
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2025-10-03 21:14:36 +0200

    wip: write thread to db

--- src/thread/handler.rs
+++ src/thread/handler.rs
@@ -1,8 +1,11 @@
-use axum::{extract::Path, Form, Json};
-use sqlx::types::Uuid;
+use axum::{Extension, Form, Json, extract::Path};
+use sqlx::{PgPool, types::Uuid};
 use std::collections::HashMap;
 
-use crate::thread::{Post, PostCreation, Thread, mock_post, mock_thread};
+use crate::{
+    board::Board,
+    thread::{Post, PostCreation, Thread, mock_post, mock_thread},
+};
 
 pub(super) async fn get_threads(Path(params): Path<HashMap<String, String>>) -> Json<Vec<Thread>> {
     let _board_name: &String = params
@@ -22,11 +25,26 @@ pub(super) async fn get_thread(Path(params): Path<HashMap<String, String>>) -> J
 
 pub(super) async fn create_thread(
     Path(params): Path<HashMap<String, String>>,
-    Form(post_creation): Form<PostCreation>
+    db_pool: Extension<PgPool>,
+    Form(post_creation): Form<PostCreation>,
 ) -> Json<Thread> {
     let board_name: &String = params
         .get("board_name")
         .expect("board_name is required to create a threads");
+
+    let board = sqlx::query_as!(
+        Board,
+        r#"
+            select board_id, name
+            from board
+            where $1 = name
+        "#,
+        board_name.clone()
+    )
+    .fetch_one(&*db_pool)
+    .await
+    .expect("Failure fetching board {board_name}");
+
     let original_post = Post {
         id: Uuid::new_v4().to_string(),
         name: post_creation.name,
@@ -34,11 +52,21 @@ pub(super) async fn create_thread(
         content: post_creation.content,
         media_url: post_creation.media_url,
     };
-    Json(Thread {
-        id: Uuid::new_v4().to_string(),
-        board_id: board_name.into(),
-        posts: vec![original_post],
-    })
+
+    let created = sqlx::query_as!(
+        Thread,
+        r#"
+        insert into thread(board_id, posts)
+                values ($1, $2)
+                returning id, board_id, posts
+        "#,
+        board.board_id,
+        Json(vec![original_post])
+    )
+    .fetch_one(&*db_pool)
+    .await;
+
+    Json(thread)
 }
 
 pub(super) async fn get_posts(Path(params): Path<HashMap<String, String>>) -> Json<Vec<Post>> {
