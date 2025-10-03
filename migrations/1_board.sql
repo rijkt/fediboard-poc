5b9e5764696785d542ecdbc2d97812a39d7fe3ee
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2025-10-03 22:34:26 +0200

    wip: work on schema/data types

--- migrations/1_board.sql
+++ migrations/1_board.sql
@@ -1,6 +1,6 @@
 create table
     "board" (
-        board_id uuid primary key default gen_random_uuid(),
+        board_id uuid primary key default gen_random_uuid (),
         name text unique not null
     );
 
@@ -10,3 +10,10 @@ values
     ('a'),
     ('b'),
     ('c');
+
+create table
+    "thread" (
+        thread_id uuid primary key default gen_random_uuid (),
+        board_id text not null,
+        posts jsonb not null
+    );
