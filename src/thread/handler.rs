c7b333765b713ed33d5c41a26a41ca0c72b178a3
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2025-10-15 17:10:41 +0200

    also implement post handlers for GET

--- src/thread/handler.rs
+++ src/thread/handler.rs
@@ -1,4 +1,8 @@
 use crate::thread::query::{self as thread_query, build_by_id_query};
+use crate::{
+    board::board_by_name_query,
+    thread::{Post, Posts, Thread},
+};
 use axum::{Extension, Form, Json, extract::Path};
 use serde::{Deserialize, Serialize};
 use sqlx::{
@@ -6,11 +10,6 @@ use sqlx::{
     types::{Json as Sqlx_json, Uuid},
 };
 use std::collections::HashMap;
-
-use crate::{
-    board::board_by_name_query,
-    thread::{Post, Posts, Thread},
-};
 
 #[derive(Serialize, Deserialize)]
 pub(crate) struct PostsView {
@@ -39,7 +38,7 @@ pub(super) async fn get_threads(
         .fetch_all(&*db_pool) // TODO: paginate
         .await
         .expect("Error reading threads");
-    let views = threads.iter().map(|t| to_view(t)).collect();
+    let views = threads.iter().map(to_thread_view).collect();
     Json(views)
 }
 
@@ -58,7 +57,7 @@ pub(super) async fn get_thread(
         .fetch_one(&*db_pool)
         .await
         .expect("Error fetching thread ");
-    Json(to_view(&thread)) // TODO: validate with board_name before returning
+    Json(to_thread_view(&thread)) // TODO: validate with board_name before returning
 }
 
 #[derive(Serialize, Deserialize)]
@@ -100,28 +99,7 @@ pub(super) async fn create_thread(
         .await
         .expect("Error creating thread");
 
-    Json(to_view(&created))
-}
-
-fn to_view(thread: &Thread) -> ThreadView {
-    let posts: &Posts = &*thread.posts;
-    ThreadView {
-        thread_id: thread.thread_id.into(),
-        board_id: thread.board_id.into(),
-        posts: PostsView {
-            posts: posts
-                .posts
-                .iter()
-                .map(|p| PostView {
-                    id: p.id.to_string(),
-                    name: p.name.clone(),
-                    subject: p.subject.clone(),
-                    content: p.content.clone(),
-                    media_url: p.media_url.clone(),
-                })
-                .collect(),
-        },
-    }
+    Json(to_thread_view(&created))
 }
 
 #[derive(Serialize, Deserialize)]
@@ -133,24 +111,75 @@ pub(super) struct PostView {
     media_url: Option<String>,
 }
 
-fn mock_post() -> PostView {
-    PostView {
-        id: "1".to_string(),
-        name: Some("anon".to_string()),
-        subject: Some("test".to_string()),
-        content: Some("hello, world".to_string()),
-        media_url: Some("https://example.com/".to_string()),
+pub(super) async fn get_posts(
+    Path(params): Path<HashMap<String, String>>,
+    db_pool: Extension<PgPool>,
+) -> Json<Vec<PostView>> {
+    let _board_name = params
+        .get("board_name")
+        .expect("board_name is required to get a thread");
+    let thread_id_str = params
+        .get("thread_id")
+        .expect("thread_id is required to fetch by id");
+    let thread_id = Uuid::parse_str(&thread_id_str).expect("thread_id needs to be Uuid");
+    let thread = build_by_id_query(thread_id)
+        .fetch_one(&*db_pool)
+        .await
+        .expect("Error fetching thread ");
+    // TODO: validate with board_name
+    let posts = &*thread.posts;
+    let post_views = posts.posts.iter().map(to_post_view).collect();
+    Json(post_views)
+}
+
+pub(super) async fn get_post(
+    Path(params): Path<HashMap<String, String>>,
+    db_pool: Extension<PgPool>,
+) -> Json<PostView> {
+    let _board_name = params
+        .get("board_name")
+        .expect("board_name is required to get a thread");
+    let thread_id_str = params
+        .get("thread_id")
+        .expect("thread_id is required to fetch by id");
+    let thread_id = Uuid::parse_str(&thread_id_str).expect("thread_id needs to be Uuid");
+    let post_id_str = params
+        .get("post_id")
+        .expect("post_id is required to fetch by id");
+    let post_id = Uuid::parse_str(&post_id_str).expect("post_id needs to be Uuid");
+
+    let thread = build_by_id_query(thread_id)
+        .fetch_one(&*db_pool)
+        .await
+        .expect("Error fetching thread ");
+    // TODO: validate with board_name
+    let posts = &*thread.posts;
+    let post = posts
+        .posts
+        .iter()
+        .find(|post| post.id == post_id)
+        .expect("thread_id must match"); // TODO: handle with 404
+    Json(to_post_view(post))
+}
+
+fn to_thread_view(thread: &Thread) -> ThreadView {
+    let posts: &Posts = &*thread.posts;
+    let post_arr = &posts.posts;
+    ThreadView {
+        thread_id: thread.thread_id.into(),
+        board_id: thread.board_id.into(),
+        posts: PostsView {
+            posts: post_arr.iter().map(to_post_view).collect(),
+        },
     }
 }
 
-pub(super) async fn get_posts(Path(params): Path<HashMap<String, String>>) -> Json<Vec<PostView>> {
-    let _thread_id = params.get("thread_id");
-    let _post_id = params.get("post_id");
-    Json(vec![mock_post()])
-}
-
-pub(super) async fn get_post(Path(params): Path<HashMap<String, String>>) -> Json<PostView> {
-    let _thread_id = params.get("thread_id");
-    let _post_id = params.get("post_id");
-    Json(mock_post())
+fn to_post_view(p: &Post) -> PostView {
+    PostView {
+        id: p.id.to_string(),
+        name: p.name.clone(),
+        subject: p.subject.clone(),
+        content: p.content.clone(),
+        media_url: p.media_url.clone(),
+    }
 }
