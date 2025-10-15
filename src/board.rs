1f0aa7b73ffc9da4156f575d3b9f235906c135d4
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2025-10-15 16:27:26 +0200

    add missing handler, connect to db

--- src/board.rs
+++ src/board.rs
@@ -1,6 +1,7 @@
-use axum::{Extension, Json, Router, routing::get};
+use axum::{Extension, Json, Router, extract::Path, routing::get};
 use serde::Serialize;
 use sqlx::{PgPool, Postgres, postgres::PgArguments, prelude::FromRow};
+use std::collections::HashMap;
 use uuid::Uuid;
 
 use crate::thread;
@@ -15,8 +16,22 @@ pub(crate) struct Board {
 pub(crate) fn routes() -> Router {
     Router::new()
         .route("/", get(get_boards))
+        .route("/{board_name}", get(get_board_by_name))
         .nest("/{board_name}/threads", thread::routes())
-        // TODO: add handler for board by name
+}
+
+async fn get_board_by_name(
+    Path(params): Path<HashMap<String, String>>,
+    db_pool: Extension<PgPool>,
+) -> Json<Board> {
+    let board_name: &String = params
+        .get("board_name")
+        .expect("board_name is required to get board by name");
+    let board = board_by_name_query(board_name)
+        .fetch_one(&*db_pool)
+        .await
+        .expect("Failure fetching boards");
+    Json(board)
 }
 
 async fn get_boards(db_pool: Extension<PgPool>) -> Json<Vec<Board>> {
