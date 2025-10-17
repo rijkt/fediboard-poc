66a08e2aad76f3e8729b3aa25212783ed1eb85fb
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2025-10-17 22:52:14 +0200

    extract post submodule

--- src/thread.rs
+++ src/thread.rs
@@ -1,16 +1,19 @@
 mod handler;
+mod post;
 mod query;
 
 use axum::{
     Router,
     routing::{get, post},
 };
-use serde::{Deserialize, Serialize};
 use sqlx::prelude::FromRow;
 use sqlx::types::Json;
 use uuid::Uuid;
 
-use crate::thread::handler::{create_thread, get_post, get_posts, get_thread, get_threads};
+use crate::thread::{
+    handler::{create_thread, get_thread, get_threads},
+    post::{Posts, get_post, get_posts},
+};
 
 pub(crate) fn routes() -> Router {
     Router::new()
@@ -19,19 +22,6 @@ pub(crate) fn routes() -> Router {
         .route("/{thread_id}", get(get_thread))
         .route("/{thread_id}/posts", get(get_posts))
         .route("/{thread_id}/posts/{post_id}", get(get_post))
-}
-
-#[derive(Serialize, Deserialize)]
-struct Post {
-    id: Uuid,
-    name: Option<String>, // poster name
-    subject: Option<String>,
-    content: Option<String>,
-    media_url: Option<String>,
-}
-#[derive(Serialize, Deserialize)]
-struct Posts {
-    posts: Vec<Post>,
 }
 
 #[derive(FromRow)]
---
+++ src/thread/post.rs
@@ -0,0 +1,88 @@
+use crate::board::validate_board_name;
+use crate::thread::handler::fetch_thread_by_id;
+use crate::thread::handler::validate_thread_id;
+use axum::Extension;
+use axum::Json;
+use axum::extract::Path;
+use axum::http::StatusCode;
+use serde::Deserialize;
+use serde::Serialize;
+use sqlx::PgPool;
+use std::collections::HashMap;
+use uuid::Uuid;
+
+#[derive(Serialize, Deserialize)]
+pub(super) struct Post {
+    pub(super) id: Uuid,
+    pub(super) name: Option<String>, // poster name
+    pub(super) subject: Option<String>,
+    pub(super) content: Option<String>,
+    pub(super) media_url: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub(super) struct Posts {
+    pub(super) posts: Vec<Post>,
+}
+
+pub(super) async fn get_posts(
+    Path(params): Path<HashMap<String, String>>,
+    db_pool: Extension<PgPool>,
+) -> Result<Json<Vec<PostView>>, StatusCode> {
+    let board_name = validate_board_name(&params)?;
+    let thread_id = validate_thread_id(&params)?;
+    let thread = fetch_thread_by_id(thread_id, board_name, db_pool).await?;
+    let post_views = thread.posts.posts.iter().map(to_post_view).collect();
+    Ok(Json(post_views))
+}
+
+pub(super) async fn get_post(
+    Path(params): Path<HashMap<String, String>>,
+    db_pool: Extension<PgPool>,
+) -> Result<Json<PostView>, StatusCode> {
+    let board_name: &str = validate_board_name(&params)?;
+    let thread_id = validate_thread_id(&params)?;
+    let post_id = validate_post_id(&params)?;
+    let thread = fetch_thread_by_id(thread_id, board_name, db_pool).await?;
+    let posts = &thread.posts.posts;
+    let matching_post = posts.iter().find(|post| post.id == post_id);
+    match matching_post {
+        Some(post) => Ok(Json(to_post_view(post))),
+        None => Err(StatusCode::NOT_FOUND),
+    }
+}
+
+pub(super) fn validate_post_id(params: &HashMap<String, String>) -> Result<Uuid, StatusCode> {
+    let post_id_param = match params.get("post_id") {
+        Some(param) => param,
+        None => return Err(StatusCode::BAD_REQUEST),
+    };
+    match Uuid::parse_str(post_id_param) {
+        Ok(parsed) => Ok(parsed),
+        Err(_) => Err(StatusCode::NOT_FOUND),
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub(super) struct PostsView {
+    pub(super) posts: Vec<PostView>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub(super) struct PostView {
+    pub(super) id: String,
+    pub(super) name: Option<String>, // poster name
+    pub(super) subject: Option<String>,
+    pub(super) content: Option<String>,
+    pub(super) media_url: Option<String>,
+}
+
+pub(super) fn to_post_view(post: &Post) -> PostView {
+    PostView {
+        id: post.id.to_string(),
+        name: post.name.clone(),
+        subject: post.subject.clone(),
+        content: post.content.clone(),
+        media_url: post.media_url.clone(),
+    }
+}
