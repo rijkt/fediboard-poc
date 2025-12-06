e569b34c810abfd1b3eab72182b7fb90ddc867c5
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2025-12-06 18:01:50 +0100

    move to appropriate module

--- src/thread.rs
+++ src/thread.rs
@@ -6,15 +6,15 @@ use axum::{
     Router,
     routing::{get, post},
 };
-use sqlx::prelude::FromRow;
 use sqlx::types::Json;
+use sqlx::{PgPool, prelude::FromRow};
 use uuid::Uuid;
 
 use crate::{
     infra::AppState,
     thread::{
         post::Posts,
-        thread_handler::{create_thread, get_thread, get_threads},
+        thread_handler::{create_thread, fetch_thread_by_id, get_thread, get_threads},
     },
 };
 
@@ -28,8 +28,43 @@ pub(crate) fn routes(app_state: AppState) -> Router {
 }
 
 #[derive(FromRow)]
-struct Thread {
+pub struct Thread {
     pub(crate) thread_id: Uuid,
     pub(crate) board_id: Uuid,
     pub(crate) posts: Json<Posts>,
+}
+
+pub enum ThreadError {
+    IdError,
+    DbError,
+}
+pub trait ThreadUseCase {
+    async fn get_thread_by_id(
+        &self,
+        thread_id: &str,
+        board_name: &str,
+    ) -> Result<Thread, ThreadError>;
+}
+
+#[derive(Clone)]
+pub struct ThreadUseCaseImpl {
+    pub db_pool: PgPool,
+}
+
+impl ThreadUseCase for ThreadUseCaseImpl {
+    async fn get_thread_by_id(
+        &self,
+        thread_id: &str,
+        board_name: &str,
+    ) -> Result<Thread, ThreadError> {
+        let uuid_result = Uuid::parse_str(thread_id);
+        let thread_uuid = match uuid_result {
+            Ok(id) => id,
+            Err(_) => return Err(ThreadError::IdError),
+        };
+        match fetch_thread_by_id(thread_uuid, board_name, &self.db_pool).await {
+            Ok(thread) => Ok(thread),
+            Err(_) => Err(ThreadError::DbError),
+        }
+    }
 }
