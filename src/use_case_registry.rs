719d45a27acef53e6225638a9edbd9e1ad08ea77
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2025-12-06 18:05:29 +0100

    register use case

--- src/use_case_registry.rs
+++ src/use_case_registry.rs
@@ -1,22 +1,31 @@
-use crate::board::{BoardUseCase, BoardUseCaseImpl};
+use crate::{
+    board::{BoardUseCase, BoardUseCaseImpl},
+    thread::{ThreadUseCase, ThreadUseCaseImpl},
+};
 
 #[derive(Clone)]
 pub struct UseCaseRegistry {
     board_use_case: BoardUseCaseImpl,
+    thread_use_case: ThreadUseCaseImpl,
 }
 
 impl UseCaseRegistry {
-    pub fn new(board_use_case: BoardUseCaseImpl) -> Self {
-        Self { board_use_case }
-    }
-
     pub fn board_use_case(&self) -> impl BoardUseCase {
         self.board_use_case.clone()
+    }
+
+    pub fn thread_use_case(&self) -> impl ThreadUseCase {
+        self.thread_use_case.clone()
     }
 }
 
 pub fn build_registry(db_pool: &sqlx::Pool<sqlx::Postgres>) -> UseCaseRegistry {
-    UseCaseRegistry::new(BoardUseCaseImpl {
-        db_pool: db_pool.clone(),
-    })
+    UseCaseRegistry {
+        board_use_case: BoardUseCaseImpl {
+            db_pool: db_pool.clone(),
+        },
+        thread_use_case: ThreadUseCaseImpl {
+            db_pool: db_pool.clone(),
+        },
+    }
 }
